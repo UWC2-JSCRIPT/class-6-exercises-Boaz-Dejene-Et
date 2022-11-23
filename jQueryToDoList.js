9f9c211600698fa895ad80278b0cf07d0e3def35
Boaz-Dejene-Et <boazdeju@gmail.com>
2022-11-23 16:20:03 +0300

    first take

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,34 @@
 /**
  * Toggles "done" class on <li> element
  */
-
+$('li').click(function() {
+  $( this ).toggleClass( "done" );
+});
 /**
  * Delete element when delete link clicked
  */
-
+$('.delete').bind('click', function(e) {
+  e.target.parentNode.remove()
+});
 /**
  * Adds new list item to <ul>
  */
 const addListItem = function(e) {
   e.preventDefault();
   const text = $('input').val();
+  var txt1 = $("<span></span>").text(text);
+  var txt2 = $("<a></a>").text("Delete").addClass('delete');;
+  var wrapper = $(`<li></li>`).append(txt1, txt2);
+  $("ul").append(wrapper);
 
+  $(wrapper).click(function() {
+    $( this ).toggleClass( "done" );
+  });
+  $(txt2).bind('click', function(e) {
+    e.target.parentNode.remove()
+  });
   // rest here...
 };
 
 // add listener for add
+$('.add-item').bind('click', addListItem);
